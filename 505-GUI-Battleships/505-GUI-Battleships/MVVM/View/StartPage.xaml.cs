00304461899ac9c4345b1e613a69e2ab04399053
xAfterLife <lisselhard@aol.de>
2022-10-19 17:11:35 +0200

    GUI + Initial Core
    Initial Page and Core Features to start on MVVM

---
+++ 505-GUI-Battleships/505-GUI-Battleships/MVVM/View/StartPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace _505_GUI_Battleships.MVVM.View;
+
+/// <summary>
+///     Interaction logic for StartPage.xaml
+/// </summary>
+public partial class StartPage : Page
+{
+    public StartPage()
+    {
+        InitializeComponent();
+    }
+}
