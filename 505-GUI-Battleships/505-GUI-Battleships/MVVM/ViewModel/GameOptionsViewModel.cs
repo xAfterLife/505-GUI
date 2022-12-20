99dd6d05ac7b52895a6c9de0e1af7121b2c35c9f
Leon Braun <leon.felix@icloud.com>
2022-12-20 17:26:36 +0100

    GameOptionsViewModel created, GameOptionsView updated, fixed typos

---
+++ 505-GUI-Battleships/505-GUI-Battleships/MVVM/ViewModel/GameOptionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using _505_GUI_Battleships.Core;
+using _505_GUI_Battleships.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace _505_GUI_Battleships.MVVM.ViewModel;
+
+public class GameOptionsViewModel : ObservableObject
+{
+    public static ICommand? AddShipCommand { get; set; }
+    public static ICommand? DeleteShipCommand { get; set; }
+}
