37a585a16667a3497cf32efb788de3c046b9dcf6
xAfterLife <lisselhard@aol.de>
2023-01-10 11:55:48 +0100

    Yes

--- 505-GUI-Battleships/505-GUI-Battleships/MVVM/View/SelectTargetPlayerView.xaml.cs
+++ 505-GUI-Battleships/505-GUI-Battleships/MVVM/View/SelectTargetPlayerView.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace _505_GUI_Battleships.MVVM.View
+namespace _505_GUI_Battleships.MVVM.View;
+
+/// <summary>
+///     Interaktionslogik für SelectTargetPlayerView.xaml
+/// </summary>
+public partial class SelectTargetPlayerView : UserControl
 {
-    /// <summary>
-    /// Interaktionslogik für SelectTargetPlayerView.xaml
-    /// </summary>
-    public partial class SelectTargetPlayerView : UserControl
+    public SelectTargetPlayerView()
     {
-        public SelectTargetPlayerView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
