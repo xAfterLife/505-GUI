55a8cb0ab8ef199d5c4091fcd187442055d90e58
Feilfile <116070348+Feilfile@users.noreply.github.com>
2023-01-12 06:54:48 +0100

    Rocket without rotation air launch

--- 505-GUI-Battleships/505-GUI-Battleships/MVVM/ViewModel/BoardAttackViewModel.cs
+++ 505-GUI-Battleships/505-GUI-Battleships/MVVM/ViewModel/BoardAttackViewModel.cs
@@ -5,14 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace _505_GUI_Battleships.MVVM.ViewModel;
@@ -126,6 +127,7 @@ internal sealed class BoardAttackViewModel: ObservableObject
 
         _playerBoard = _gameService.GameBoard!.Board;
         _playerBoard.MouseLeftButtonDown += MouseLeftButtonDownHandler;
+        _playerBoard.ClipToBounds = false;
         Grid.SetRow(_playerBoard, 1);
         Grid.SetColumn(_playerBoard, 1);
 
@@ -136,49 +138,88 @@ internal sealed class BoardAttackViewModel: ObservableObject
     {
         var clickPosition = e.GetPosition((IInputElement)sender);
         clickPosition.X = Math.Round(clickPosition.X - 0.5);
+        if ( clickPosition.X < 0)
+            clickPosition.X = 0;
         clickPosition.Y = Math.Round(clickPosition.Y - 0.5);
+        if (clickPosition.Y > _boardDimensions.Height - 1)
+            clickPosition.Y = _boardDimensions.Height - 1;
         Trace.WriteLine(clickPosition.ToString());
-        TestAnimation();
+        TestAnimation(clickPosition);
     }
 
-    private void TestAnimation()
+    private void TestAnimation(Point clickPosition)
     {
-        Rectangle rect = new Rectangle()
+
+        BitmapImage imageSource = new BitmapImage(new Uri("pack://application:,,,/505-GUI-Battleships;component/Ressources/Rocket.png", UriKind.RelativeOrAbsolute));
+
+        Image rect1 = new Image()
         {
             Width = 1,
-            Height = 1,
-            Fill = Brushes.Red,
+            Height = 1, 
+            Source = imageSource
         };
 
-        TranslateTransform tt = new TranslateTransform(5, 5);
-        rect.RenderTransform = tt;
-        _playerBoard.Children.Add(rect);
-
-        DoubleAnimation animationX = new DoubleAnimation()
-        {
-            From = 0,
-            To = 10,
-            Duration = TimeSpan.FromSeconds(10),
-        };
-
-        DoubleAnimation animationY = new DoubleAnimation()
-        {
-            From = 0,
-            To = 10,
-            Duration = TimeSpan.FromSeconds(10),
-        };
-
-        Storyboard.SetTarget(animationX, tt);
-        Storyboard.SetTargetProperty(animationX, new PropertyPath("X"));
-        Storyboard.SetTarget(animationY, tt);
-        Storyboard.SetTargetProperty(animationY, new PropertyPath("Y"));
-
-        // Create a storyboard to contain the animation
+        TranslateTransform tt = new TranslateTransform();
+        rect1.RenderTransform = tt;
+        //RotateTransform rotateTransform = new RotateTransform();
+        //rect1.RenderTransform = rotateTransform;
+        //rotateTransform.Angle = 180;
+        int index = _playerBoard.Children.Add(rect1);
+        Image rect = (Image)_playerBoard.Children[index];
         Storyboard storyboard = new Storyboard();
-        storyboard.Children.Add(animationX);
-        storyboard.Children.Add(animationY);
 
-        // Start the animation
+        double g = 90.8;
+        double h = 0;
+        double k = _boardDimensions.Height/2 - rect.Height;
+        double v = 50;
+        double angle = 45;
+        double rad = angle * Math.PI / 180;
+        double t = (2 * v * Math.Sin(rad)) / g;
+        double x = (v * Math.Cos(rad)) * t;
+
+        // Create a DoubleAnimation to animate the rectangle's Left property
+        var doubleAnimationX1 = new DoubleAnimation();
+        doubleAnimationX1.From = -4;
+        doubleAnimationX1.To = (clickPosition.X + 3) / 2 - 3;
+        doubleAnimationX1.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+        var doubleAnimationX2 = new DoubleAnimation();
+        doubleAnimationX2.From = (clickPosition.X + 3) / 2 - 3;
+        doubleAnimationX2.To = clickPosition.X;
+        doubleAnimationX2.BeginTime = doubleAnimationX1.Duration.TimeSpan;
+        doubleAnimationX2.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+        // Create a second DoubleAnimation to animate the rectangle's Top property
+        var doubleAnimationY1 = new DoubleAnimation();
+        doubleAnimationY1.From = 1;
+        doubleAnimationY1.To = clickPosition.Y + 15;
+        doubleAnimationY1.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+        var doubleAnimationY2 = new DoubleAnimation();
+        doubleAnimationY2.From = clickPosition.Y + 15;
+        doubleAnimationY2.To = clickPosition.Y;
+        doubleAnimationY2.BeginTime = doubleAnimationY1.Duration.TimeSpan;
+        doubleAnimationY2.Duration = new Duration(TimeSpan.FromSeconds(1));
+
+        // Create a Storyboard.TargetName and Storyboard.TargetProperty
+        Storyboard.SetTarget(doubleAnimationX1, rect1);
+        Storyboard.SetTargetProperty(doubleAnimationX1, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.X)"));
+        Storyboard.SetTarget(doubleAnimationY1, rect1);
+        Storyboard.SetTargetProperty(doubleAnimationY1, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)"));
+
+        Storyboard.SetTarget(doubleAnimationX2, rect1);
+        Storyboard.SetTargetProperty(doubleAnimationX2, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.X)"));
+        Storyboard.SetTarget(doubleAnimationY2, rect1);
+        Storyboard.SetTargetProperty(doubleAnimationY2, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)"));
+
+        // Add the animation to the storyboard
+        storyboard.Children.Add(doubleAnimationX1);
+        storyboard.Children.Add(doubleAnimationX2);
+        storyboard.Children.Add(doubleAnimationY1);
+        storyboard.Children.Add(doubleAnimationY2);
+
+        // Start the storyboard
         storyboard.Begin();
+
     }
 }
