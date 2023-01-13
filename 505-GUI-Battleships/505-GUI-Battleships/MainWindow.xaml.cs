0974aaf218dec29d2d7f34aebccd24a02a682488
xAfterLife <lisselhard@aol.de>
2023-01-13 14:21:30 +0100

    Make Window Stick to AspectRatio

--- 505-GUI-Battleships/505-GUI-Battleships/MainWindow.xaml.cs
+++ 505-GUI-Battleships/505-GUI-Battleships/MainWindow.xaml.cs
@@ -7,8 +7,25 @@ namespace _505_GUI_Battleships;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private const double AspectRatio = 1.66666666667;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+    {
+        base.OnRenderSizeChanged(sizeInfo);
+
+        if ( sizeInfo.HeightChanged )
+        {
+            Width = sizeInfo.NewSize.Height * AspectRatio;
+        }
+
+        if ( sizeInfo.WidthChanged )
+        {
+            Height = sizeInfo.NewSize.Width / AspectRatio;
+        }
     }
 }
