0bc22dcd4d34670968839405f0705429b3bacd11
Leon Braun <leon.felix@icloud.com>
2023-01-14 10:29:33 +0100

    push time

--- 505-GUI-Battleships/505-GUI-Battleships/MVVM/View/EndOfGameView.xaml.cs
+++ 505-GUI-Battleships/505-GUI-Battleships/MVVM/View/EndOfGameView.xaml.cs
@@ -1,14 +1,28 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace _505_GUI_Battleships.MVVM.View;
-
-/// <summary>
-///     Interaktionslogik für EndOfGameView.xaml
-/// </summary>
-public partial class EndOfGameView : UserControl
+namespace _505_GUI_Battleships.MVVM.View
 {
-    public EndOfGameView()
+    /// <summary>
+    /// Interaktionslogik für EndOfGameView.xaml
+    /// </summary>
+    public partial class EndOfGameView : UserControl
     {
-        InitializeComponent();
+        public EndOfGameView()
+        {
+            InitializeComponent();
+        }
     }
 }
